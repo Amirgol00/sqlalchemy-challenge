4ba9be858660ef59c9d073dee0f39202a700f1ed
Amirgol00 <amir.golshan.al@gmail.com>
2024-01-22 02:23:29 -0500

    Update app.py

--- app.py
+++ app.py
@@ -5,29 +5,101 @@ from sqlalchemy.ext.automap import automap_base
 import datetime as dt
 
 # Create engine to hawaii.sqlite
-engine = create_engine("sqlite:///hawaii.sqlite")
+engine = create_engine("sqlite:///Resources/hawaii.sqlite")
 
 # Reflect an existing database into a new model
 Base = automap_base()
 # Reflect the tables
 Base.prepare(engine, reflect=True)
+print(Base.classes.keys())
 
-# Save references to each table
-Station = Base.classes.station
-Measurement = Base.classes.measurement
 
-app = Flask(__name__)
+# # Save references to each table
+# Station = Base.classes.station
+# Measurement = Base.classes.measurement
 
-@app.route("/")
-def home():
-    return (
-        f"Welcome to the Climate API!<br/>"
-        f"Available Routes:<br/>"
-        f"/api/v1.0/precipitation<br/>"
-        f"/api/v1.0/stations<br/>"
-        f"/api/v1.0/tobs<br/>"
-        f"/api/v1.0/&lt;start&gt;<br/>"
-        f"/api/v1.0/&lt;start&gt;/&lt;end&gt;<br/>"
-    )
+# app = Flask(__name__)
+
+# @app.route("/")
+# def home():
+#     return (
+#         f"Welcome to the Climate API!<br/>"
+#         f"Available Routes:<br/>"
+#         f"/api/v1.0/precipitation<br/>"
+#         f"/api/v1.0/stations<br/>"
+#         f"/api/v1.0/tobs<br/>"
+#         f"/api/v1.0/&lt;start&gt;<br/>"
+#         f"/api/v1.0/&lt;start&gt;/&lt;end&gt;<br/>"
+#     )
+
+
+
+# @app.route("/api/v1.0/precipitation")
+# def precipitation():
+#     session = Session(engine)
+#     # Calculate the date one year from the last date in data set
+#     one_year_ago = dt.date(2017, 8, 23) - dt.timedelta(days=365)
+#     # Perform a query to retrieve the data and precipitation scores
+#     results = session.query(Measurement.date, Measurement.prcp).\
+#         filter(Measurement.date >= one_year_ago).all()
+#     session.close()
+
+#     # Convert list of tuples into a dictionary
+#     precipitation = {date: prcp for date, prcp in results}
+#     return jsonify(precipitation)
+
+
+# @app.route("/api/v1.0/stations")
+# def stations():
+#     session = Session(engine)
+#     # Query all stations
+#     results = session.query(Station.station).all()
+#     session.close()
+
+#     # Convert list of tuples into normal list
+#     stations = list(np.ravel(results))
+#     return jsonify(stations)
+
+
+# @app.route("/api/v1.0/tobs")
+# def tobs():
+#     session = Session(engine)
+#     # Query the last year of temperature observation data for the most active station
+#     most_active_station = 'USC00519281'  # Replace with your most active station
+#     one_year_ago = dt.date(2017, 8, 23) - dt.timedelta(days=365)
+#     results = session.query(Measurement.tobs).\
+#         filter(Measurement.station == most_active_station).\
+#         filter(Measurement.date >= one_year_ago).all()
+#     session.close()
+
+#     # Convert list of tuples into normal list
+#     tobs = list(np.ravel(results))
+#     return jsonify(tobs)
+
+
+
+# @app.route("/api/v1.0/<start>")
+# def start(start):
+#     session = Session(engine)
+#     results = session.query(func.min(Measurement.tobs), func.avg(Measurement.tobs), func.max(Measurement.tobs)).\
+#         filter(Measurement.date >= start).all()
+#     session.close()
+
+#     # Convert list of tuples into normal list
+#     temps = list(np.ravel(results))
+#     return jsonify(temps)
+
+# @app.route("/api/v1.0/<start>/<end>")
+# def start_end(start, end):
+#     session = Session(engine)
+#     results = session.query(func.min(Measurement.tobs), func.avg(Measurement.tobs), func.max(Measurement.tobs)).\
+#         filter(Measurement.date >= start).\
+#         filter(Measurement.date <= end).all()
+#     session.close()
+
+#     # Convert list of tuples into normal list
+#     temps = list(np.ravel(results))
+#     return jsonify(temps)
+
 
 
